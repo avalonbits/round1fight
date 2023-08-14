c2cb93032309bcab1cd472b0aca8b58417c614f5
Igor Cananea <icc@avalonbits.com>
2023-08-14 10:08:12 -0400

    Shorter http error creation.

--- endpoints/api/api.go
+++ endpoints/api/api.go
@@ -14,17 +14,21 @@ func New() *Person {
 }
 
 func (h *Person) Create(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented, "")
+	return httpErr(http.StatusNotImplemented, "")
 }
 
 func (h *Person) Get(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented, "")
+	return httpErr(http.StatusNotImplemented, "")
 }
 
 func (h *Person) Search(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented, "")
+	return httpErr(http.StatusNotImplemented, "")
 }
 
 func (h *Person) Count(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented, "")
+	return httpErr(http.StatusNotImplemented, "")
+}
+
+func httpErr(code int, msg string) *echo.HTTPError {
+	return echo.NewHTTPError(code, msg)
 }

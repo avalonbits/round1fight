72e5dbf281e67d0817939c55532288264a0a546f
Igor Cananea <icc@avalonbits.com>
2023-08-15 18:26:28 -0400

    Connect to db.

--- cmd/round1fight/main.go
+++ cmd/round1fight/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/avalonbits/round1fight/endpoints/api"
 	"github.com/avalonbits/round1fight/service/person"
 	"github.com/avalonbits/round1fight/storage/pg/repo"
+	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	svc := person.New(repo.New(nil))
+	db, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	svc := person.New(repo.New(db))
 	person := api.New(svc)
 	e := echo.New()
 	e.POST("/pessoas", person.Create)
